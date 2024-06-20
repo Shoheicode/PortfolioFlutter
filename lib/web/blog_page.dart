fd480222f05c1b138b4760a9a271f0d982fde8f5
Shoheicode <40525329+Shoheicode@users.noreply.github.com>
2024-06-20 15:45:37 -0700

    Added blog page

---
+++ lib/web/blog_page.dart
@@ -0,0 +1,213 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:portfolio/components.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class BlogPage extends StatefulWidget{
+
+  const BlogPage({super.key});
+
+
+  @override
+  State<StatefulWidget> createState() =>_BlogPage();
+
+}
+
+class _BlogPage extends State<BlogPage>{
+  urlLauncher(String imgPath, String url){
+    return IconButton(
+      icon: Image.asset(imgPath, width: 35,filterQuality: FilterQuality.high,),
+      //Async allows for urls launched
+      onPressed: () async {
+        await launchUrl(Uri.parse(url));
+      },
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    //Gets the height of the device being used
+    var heightDevice = MediaQuery.of(context).size.height;
+    var widthDevice = MediaQuery.of(context).size.width;
+
+    // TODO: implement build
+    return Scaffold(
+      drawer:Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 70,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("J.png"),
+            ),
+            SizedBox(height: 15,),
+            SansBold("Jason Irie", 30),
+            SizedBox(height: 15.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                urlLauncher("linkedin.png", "https://www.linkedin.com/in/jason-irie-2bb2b0243/"),
+                IconButton(
+                  icon: SvgPicture.asset("github.svg", color: Colors.black, width: 35.0),
+                  onPressed: () async {
+                    await launchUrl(Uri.parse("https://github.com/Shoheicode"));
+                  },
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
+      backgroundColor: Colors.white,
+      //The Main body of the web application
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 800.0,
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(
+                size: 25.0,
+                color: Colors.black,
+                
+              ),
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Container(
+                  decoration: BoxDecoration(
+                    color:Colors.black,
+                    borderRadius: BorderRadius.circular(3.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: AbelText(
+                    text: "Welcome to my blog", 
+                    size: 60.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ), 
+                  
+                ),
+                
+                background: Image.asset("blog.jpg", filterQuality: FilterQuality.high, fit: BoxFit.cover,),
+                
+              ),
+              title: Row(
+                children: [
+                  Spacer(flex: 3,),
+                  TabsWeb("Home", color: Colors.white,),
+                  Spacer(),
+                  TabsWeb("Projects", color: Colors.white,),
+                  Spacer(),
+                  TabsWeb("Blog", color: Colors.white,),
+                  Spacer(),
+                  TabsWeb("About", color: Colors.white,),
+                  Spacer(),
+                  TabsWeb("Contact", color: Colors.white,)
+                ],
+              ),
+            )
+          ];
+        },
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height:30.0),
+              SansBold("Contact Me", 30.0),
+              SizedBox(height:30.0),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+
+                      children: [
+                        TextForm(heading: "First Name", width: 350, hintText: "Please enter first name", maxLine: 1,),
+                        SizedBox(height: 15,),
+                        TextForm(heading: "Email", width: 350, hintText: "Please enter in email", maxLine: 1,)
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        TextForm(heading: "Last Name", width: 350, hintText: "Please enter in laster name"),
+                        SizedBox(height: 15,),
+                        TextForm(heading: "Phone Number", width: 350, hintText: "Please enter in your phone number", maxLine: 1,)
+                      ],
+                    )
+                  ],
+                ),
+                TextForm(
+                  heading: "Message", 
+                  width: widthDevice/1.5, 
+                  hintText: "Please type your message", 
+                  maxLine: 10,
+                ),
+                SizedBox(height: 30,),
+                MaterialButton(
+                  elevation: 20.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                  height: 60.0,
+                  minWidth: 200.0,
+                  color:Colors.blueAccent,
+                  child:SansBold("Submit", 20.0),
+                  onPressed: (){
+
+                  }
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BlogPost extends StatefulWidget{
+
+  const BlogPost({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => _BlogPostState();
+
+}
+
+class _BlogPostState extends State<BlogPost>{
+  bool expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(3.0)
+                  ),
+                  child: AbelText(
+                    text: "Who is Dash?",
+                    size: 25.0,
+                    color: Colors.white
+                  ),
+
+                )
+              ],
+            )
+          ]
+        ),
+      ),
+    );
+  }
+  
+}
