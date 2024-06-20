d08ef682d726130215bea87f40a9aef6601a085e
Shoheicode <40525329+Shoheicode@users.noreply.github.com>
2024-06-20 15:53:34 -0700

    Added blog post template

--- lib/web/blog_page.dart
+++ lib/web/blog_page.dart
@@ -111,55 +111,11 @@ class _BlogPage extends State<BlogPage>{
             )
           ];
         },
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height:30.0),
-              SansBold("Contact Me", 30.0),
-              SizedBox(height:30.0),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-
-                      children: [
-                        TextForm(heading: "First Name", width: 350, hintText: "Please enter first name", maxLine: 1,),
-                        SizedBox(height: 15,),
-                        TextForm(heading: "Email", width: 350, hintText: "Please enter in email", maxLine: 1,)
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        TextForm(heading: "Last Name", width: 350, hintText: "Please enter in laster name"),
-                        SizedBox(height: 15,),
-                        TextForm(heading: "Phone Number", width: 350, hintText: "Please enter in your phone number", maxLine: 1,)
-                      ],
-                    )
-                  ],
-                ),
-                TextForm(
-                  heading: "Message", 
-                  width: widthDevice/1.5, 
-                  hintText: "Please type your message", 
-                  maxLine: 10,
-                ),
-                SizedBox(height: 30,),
-                MaterialButton(
-                  elevation: 20.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                  height: 60.0,
-                  minWidth: 200.0,
-                  color:Colors.blueAccent,
-                  child:SansBold("Submit", 20.0),
-                  onPressed: (){
-
-                  }
-                )
-            ],
-          ),
-        ),
+        body: ListView(
+          children: [
+            BlogPost(),
+          ],
+        )
       ),
     );
   }
@@ -199,9 +155,15 @@ class _BlogPostState extends State<BlogPost>{
                     text: "Who is Dash?",
                     size: 25.0,
                     color: Colors.white
-                  ),
-
-                )
+                  )
+                ),
+                IconButton(
+                    icon: Icon(Icons.arrow_drop_down_circle_outlined),
+                    onPressed: (){
+                      expanded = !expanded;
+                    },
+                    color: Colors.black,
+                ),
               ],
             )
           ]
