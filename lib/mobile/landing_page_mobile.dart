66fcc27bb1f1be062694720dcba3a7a46719ad63
ShoheiCode <ucodejasonirie@gmail.com>
2025-02-25 14:00:17 -0800

    updated text

--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -200,11 +200,7 @@ class _LandingPageMobileState extends State<LandingPageMobile> {
                       height: 15,
                     ),
                     Sans(
-                        "Hello! I'm Jason Irie. I specialize in C++ and making applications using C++.",
-                        15.0,
-                        TextAlign.center),
-                    Sans(
-                        "I strive to ensure my projects and my work is the best to its ability and striving to utilize my abilities to help others. I have created many projects that",
+                        "Hello! I'm Jason Irie. I specialize in C++ and making applications using C++. I strive to ensure my projects and my work is the best to its ability and striving to utilize my abilities to help others. I have created many projects that",
                         15.0,
                         TextAlign.center),
                     Sans(
